8e5c90e581a6bff7737801453e453422ee05d0b6
lucas de almeida <lucasde.almeida@hotmail.com>
2021-10-18 01:39:03 -0300

    adicionado controller dos favorites ao home

--- lib/modules/home/home_page_bindigns.dart
+++ lib/modules/home/home_page_bindigns.dart
@@ -1,3 +1,4 @@
+import 'package:comics_marvel/modules/favorites/favoriters_controller.dart';
 import 'package:comics_marvel/modules/home/home_page_controller.dart';
 
 import 'package:get/get.dart';
@@ -6,5 +7,6 @@ class HomePageBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => HomePageController(), fenix: true);
+    Get.put(FavoritersController(), permanent: true);
   }
 }
