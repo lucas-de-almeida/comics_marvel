767f862440cdad903e844669b3ede5423939b92b
lucas de almeida <lucasde.almeida@hotmail.com>
2021-10-18 01:33:26 -0300

    alterada forma de inicizaçao do main nao sera feita traduçao

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       getPages: AppPages.pages,
-//       home: const HomePage(),
-//       initialBinding: HomePageBinding(),
-//       fallbackLocale: const Locale('pt', 'BR'),
-//     );
-//   }
-//}
